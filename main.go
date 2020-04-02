6b185058c4e4d03063396054c04c995e44f6572b
scorpion <workingscorpion@gmail.com>
2020-04-02 19:22:45 +0900

    keep going

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ydlamia/dictionary_golang/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{}
+	fmt.Println(dictionary)
+}
---
+++ mydict/mydict.go
@@ -0,0 +1,7 @@
+package mydict
+
+// type Money int
+// Money(1) = money=1
+
+// Dictionary
+type Dictionary map[string]string
